a054283f2a525796b73b8677c6915073b62981bd
Niklas008 <153507660+Niklas008@users.noreply.github.com>
2024-01-15 11:46:08 +0100

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,15 @@
+input.onPinTouchEvent(TouchPin.P1, input.buttonEventDown(), function () {
+    basic.setLedColor(0xff0080)
+})
+input.onPinTouchEvent(TouchPin.P0, input.buttonEventDown(), function () {
+    basic.setLedColor(0x00ff00)
+})
+input.onPinTouchEvent(TouchPin.P2, input.buttonEventDown(), function () {
+    basic.setLedColor(0xffff00)
+})
+input.onPinTouchEvent(TouchPin.P3, input.buttonEventDown(), function () {
+    basic.setLedColor(0x00ffff)
+})
 basic.forever(function () {
 	
 })
